72b3f900b0e555dcce6e12feea0b6d03e3d37cbd
Marion Chatard <marion.chatard@etu.minesparis.psl.eu>
2023-12-12 15:24:05 +0100

    version 3

--- snake.py
+++ snake.py
@@ -6,6 +6,8 @@ pygame.init()
 
 screen = pygame.display.set_mode( (width, height) )
 clock = pygame.time.Clock()
+rose = (249,66,158)
+snake_size = 8
 
 while True:
 
@@ -15,6 +17,10 @@ while True:
 # Process new events (keyboard, mouse)
 for event in pygame.event.get():
     pass # do nothing for the moment
+
+def our_snake(snake_size, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, rose, [x[0], x[1], snake_size, snake_size])
 
 screen.fill( (0, 255, 0) )
 pygame.quit()
