631d5a4c46f6cf0452e57c39341faae7192a153c
Marion Chatard <marion.chatard@etu.minesparis.psl.eu>
2023-12-12 15:07:17 +0100

    nouvelle version snake

--- snake.py
+++ snake.py
@@ -12,6 +12,9 @@ while True:
     # Wait one second, starting from last display or now
     clock.tick(1)
 
+# Process new events (keyboard, mouse)
+for event in pygame.event.get():
+    pass # do nothing for the moment
 
 screen.fill( (0, 255, 0) )
 pygame.quit()
