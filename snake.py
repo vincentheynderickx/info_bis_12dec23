d9cb716da5a7256663b3c6e3c1c0dc2e3043a2b0
Marion Chatard <marion.chatard@etu.minesparis.psl.eu>
2023-12-14 10:11:56 +0100

    nvl version

--- snake.py
+++ snake.py
@@ -9,8 +9,7 @@ clock = pygame.time.Clock()
 pink = (249,66,158)
 white = (255, 255, 255)
 black = (0, 0, 0)
-snake_size = 3
-snake_speed = 10
+
 
 
 square_size = 20
@@ -56,18 +55,18 @@ def gameLoop():
     x1_change = 0
     y1_change = 0
 
+    snake_size = 3
+    snake_speed = 10
     snake_list = []
-    length_of_snake = 1
 
     score = 0
 
-    foodx = round(random.randrange(0, width - snake_size) / 10) * 10
-    foody = round(random.randrange(0, height - snake_size) / 10) * 10
+    foodx = round(random.randrange(0, width - snake_size) / 10.0) * 10.0
+    foody = round(random.randrange(0, height - snake_size) / 10.0) * 10.0
 
     while not game_over:
 
         while game_close == True:
-            window.fill(black)
             our_snake(snake_size, snake_list)
             pygame.display.update()
             for event in pygame.event.get():
@@ -99,13 +98,12 @@ def gameLoop():
             game_close = True
         x1 += x1_change
         y1 += y1_change
-        window.fill(black)
         pygame.draw.rect(window, pink, [foodx, foody, snake_size, snake_size])
         snake_head = []
         snake_head.append(x1)
         snake_head.append(y1)
         snake_list.append(snake_head)
-        if len(snake_list) > length_of_snake:
+        if len(snake_list) > snake_size:
             del snake_list[0]
 
         for x in snake_list[:-1]:
@@ -119,10 +117,11 @@ def gameLoop():
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, width - snake_size) / 10.0) * 10.0
             foody = round(random.randrange(0, height - snake_size) / 10.0) * 10.0
-            length_of_snake += 1
+            snake_size += 1
             score +=1
             pygame.display.set_caption('Score du Snake = ',score)
             snake_speed = snake_speed + 1
+        
 
 
         clock.tick(snake_speed)
